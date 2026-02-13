f083ca2567076ef9d3089fe5c10a6e67ec165881
debian-taketook <taketook34@gmail.com>
2026-02-13 14:58:40 +0200

    Issue #1: Refactiring main() function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time, threading, json, os, signal, sys
 from src.client_service import ClientManager
 from src.video_render import videoPlayerTask
-from src.device_info import deviceInfoTask
+from src.device_info import MQTTManager
 import netifaces
 
 CLIENT_NAME = "PC_" + netifaces.ifaddresses('wlp98s0')[netifaces.AF_INET][0]['addr']
@@ -10,6 +10,7 @@ MANAGMENT_TOPIC = "test/topic"
 broker = "192.168.0.104" # Можна замінити на IP твого ПК
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, CLIENT_NAME)
 client_manager = ClientManager(client, MANAGMENT_TOPIC)
+mqtt_manager = MQTTManager(client_manager, client)
 program_stop_signal = threading.Event()
 
 def signal_handler(sig, frame):
@@ -22,35 +23,26 @@ def signal_handler(sig, frame):
     
     sys.exit(0)
 
-def on_message(client, userdata, msg):
-    payload_str = msg.payload.decode("utf-8")
-    #print(payload_str)
-    data = json.loads(payload_str)
-    #print(data)
-
-    sender = data.get('sender')
-    if "CAM_" in sender:
-        #print(payload_str)
-        if not client_manager.check_client(sender):
-            client_manager.add_client(sender)
-
 def main():
     signal.signal(signal.SIGINT, signal_handler)
 
-    client.on_message = on_message
+    #client.on_message = on_message
     client.connect(broker, 1883)
     client.subscribe(MANAGMENT_TOPIC)
 
     global infoThread
     global videoPlayerThread
 
-    infoThread = threading.Thread(target=deviceInfoTask, args=(client_manager, program_stop_signal))
+    infoThread = threading.Thread(target=mqtt_manager.deviceInfoTask, args=(program_stop_signal,))
     infoThread.start()
+
+    mqttLoopThread = threading.Thread(target=mqtt_manager.mqttLoopTask, args=(program_stop_signal,), daemon=True)
+    mqttLoopThread.start()
 
     videoPlayerThread = threading.Thread(target=videoPlayerTask, args=(client_manager, program_stop_signal))
     videoPlayerThread.start()
 
-    client.loop_forever()
+    #client.loop_forever()
 
 if __name__ == "__main__":
     main()
--- src/device_info.py
+++ src/device_info.py
@@ -1,23 +1,46 @@
 from src.client_service import ClientManager, Client
-import time
+import time, json
 
-def deviceInfoTask(client_manager_, stop_event):
-    check_counter = 0
-    while not stop_event.is_set():
-        client_manager_.increase_timers()
-        time.sleep(0.01)
-        check_counter += 1
+class MQTTManager:
+    def __init__(self, client_manager, mqtt_client):
+        self._mqtt_client = mqtt_client
+        self._client_manager = client_manager
+        # self._stop_event = stop_event
 
-        if check_counter == 100:
-            device_info = client_manager_.show_clients_list()
-            for device in device_info.keys():
-                print(f"Client {device}: {device_info[device]}ms; ", end="")
-            print()
+        self._mqtt_client.on_message = self._on_message
+    
+    def _on_message(self, client, userdata, msg):
+        payload_str = msg.payload.decode("utf-8")
+        #print(payload_str)
+        data = json.loads(payload_str)
+        #print(data)
+
+        sender = data.get('sender')
+        if "CAM_" in sender:
+            #print(payload_str)
+            if not self._client_manager.check_client(sender):
+                self._client_manager.add_client(sender)
+
+    def mqttLoopTask(self, stop_event):
+        self._mqtt_client.loop_forever()
+    
+    def deviceInfoTask(self, stop_event):
+        check_counter = 0
+        while not stop_event.is_set():
+            self._client_manager.increase_timers()
+            time.sleep(0.01)
+            check_counter += 1
+
+            if check_counter == 100:
+                device_info = self._client_manager.show_clients_list()
+                for device in device_info.keys():
+                    print(f"Client {device}: {device_info[device]}ms; ", end="")
+                print()
+                    
+                for i in device_info.keys():
+                    if device_info[i] >= 1200:
+                        self._client_manager.del_client(i)
                 
-            for i in device_info.keys():
-                if device_info[i] >= 1200:
-                    client_manager_.del_client(i)
+                check_counter = 0
             
-            check_counter = 0
-        
-    print("Closing receiving info from devices")
+        print("Closing receiving info from devices")
